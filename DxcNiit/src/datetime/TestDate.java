9f9a196577756cf861aba08556223e1f489a45f4
abdul <abdul.tanveer@gmail.com>
2021-04-06 15:18:37 +0530

    year isleap

--- DxcNiit/src/datetime/TestDate.java
+++ DxcNiit/src/datetime/TestDate.java
@@ -4,19 +4,18 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Period;
+import java.time.Year;
 
 public class TestDate {
 	
 	public static void main(String[] args) {
-		LocalDate today = LocalDate.now();
-		LocalDate dob = LocalDate.of(1981, 01, 01);
-		Period period = Period.between(dob, today);
-		LocalDate lastDay = dob.plusYears(60);
-		Period periodLeft = Period.between(today, lastDay);
-
-		System.out.printf("no of years = %d, no of months = %d, no of days = %d",period.getYears(),period.getMonths(),period.getDays());
-		System.out.printf("\n no of years left = %d, no of months left = %d, no of days left= %d",periodLeft.getYears(),periodLeft.getMonths(),periodLeft.getDays());
-
+		Year year = Year.of(2020);
+		if(year.isLeap()) {
+			System.out.println("leap year");
+		}
+		else {
+			System.out.println("not a leap year");
+		}
 	}
 
 	private static int add(int i, int j) {
