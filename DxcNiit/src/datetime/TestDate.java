9964e7ef4f4a4c672d0739dc70f7c78f432415ee
abdul <abdul.tanveer@gmail.com>
2021-04-06 15:23:25 +0530

    zoneid, zoneDateTime

--- DxcNiit/src/datetime/TestDate.java
+++ DxcNiit/src/datetime/TestDate.java
@@ -5,17 +5,18 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Period;
 import java.time.Year;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 public class TestDate {
 	
 	public static void main(String[] args) {
-		Year year = Year.of(2020);
-		if(year.isLeap()) {
-			System.out.println("leap year");
-		}
-		else {
-			System.out.println("not a leap year");
-		}
+		ZoneId zone = ZoneId.systemDefault();
+		System.out.println(zone);
+		
+		ZoneId officZoneId = ZoneId.of("America/Los_Angeles");
+		ZonedDateTime zdt = ZonedDateTime.now(officZoneId);
+		System.out.println("date and time in los angeles now is"+zdt);
 	}
 
 	private static int add(int i, int j) {
