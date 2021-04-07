e707362046ffc6bbd1f97f1053d1f780d90774ed
abdul <abdul.tanveer@gmail.com>
2021-04-07 12:30:03 +0530

    custom serialization for both un n pwd

--- DxcNiit/src/serialization/Account.java
+++ DxcNiit/src/serialization/Account.java
@@ -10,12 +10,15 @@ public class Account implements Serializable{
 	transient String password = "ansari";
 
 	private void writeObject(ObjectOutputStream oos) throws ClassNotFoundException,IOException{
-		
-		oos.writeUTF("123"+password);
+		oos.writeUTF("123"+userName+","+"456"+password);
+
+		//oos.writeUTF(",456"+password);
 	}
 	
 	private void readObject(ObjectInputStream ois) throws IOException {
-		this.password = ois.readUTF().substring(3);
+		String[] unPwd = ois.readUTF().split(",");
+		this.userName = unPwd[0].substring(3);
+		this.password = unPwd[1].substring(3);
 	}
 	
 	
