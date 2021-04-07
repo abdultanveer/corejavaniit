0ac1379f8c1f614326feeb78d78f3c0eedaf50d9
abdul <abdul.tanveer@gmail.com>
2021-04-07 11:24:07 +0530

    Instanceof

---
+++ DxcNiit/src/serialization/InstanceOfDemo.java
@@ -0,0 +1,26 @@
+package serialization;
+
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+
+public class InstanceOfDemo {
+	
+	public static void main(String[] args) throws Exception{
+		FileInputStream fis = new FileInputStream("animals.txt");
+		ObjectInputStream ois = new ObjectInputStream(fis);
+		Object o = ois.readObject();
+		ois.close();
+		
+		if(o instanceof Cat) {
+			Cat c1 = (Cat)o;
+		}
+		else if(o instanceof Dog) {
+			Dog d1 = (Dog)o;
+		}
+		else 		if(o instanceof Rat) {
+			Rat r1 = (Rat)o;
+		}
+
+	}
+
+}
